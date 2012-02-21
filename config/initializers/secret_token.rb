f07410af71a6b09f944a284d0a74f5fde4bd8d8d
alec <alec.poluk@gmail.com>
2012-02-21 15:25:21 +0100

    Debut appli

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pilou::Application.config.secret_token = '95b0ee328ca5a93152660a60a11e5aed9600c5432510c365b15531b7cd73b62842cd91d95515a680b6b24e30b0d682d1a41246b866e2ddcfaf09f5bd79427101'
